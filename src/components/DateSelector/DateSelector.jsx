1423b25afc1f122a5f2212281a4ab8310a96b2d1
Ted30 <teddymarcenaje@gmail.com>
2024-04-25 10:14:06 +0800

    build config

--- src/components/DateSelector/DateSelector.jsx
+++ src/components/DateSelector/DateSelector.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-import dayjs, { Dayjs } from '`dayjs`'
+import dayjs, { Dayjs } from 'dayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { DatePicker } from '@mui/x-date-pickers/DatePicker'
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
-import { createProxyMiddleware } from 'http-proxy-middleware'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,16 +11,12 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://10.5.5.19:8000',
+                target: 'http://202.124.157.19:8000',
                 changeOrigin: true,
+                secure: false,
+                // rewrite: (path) => path.replace(/^\/api/, ''),
             },
         },
-        // middleware: createProxyMiddleware('/api', {
-        //     target: 'http://10.5.5.19:8000',
-        //     changeOrigin: true,
-        //     pathRewrite: {
-        //         '^/api': '',
-        //     },
-        // }),
     },
+    base: './',
 })
