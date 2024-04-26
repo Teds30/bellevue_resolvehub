5b334852ebe117e2ac7fd880a4283305d5c82b58
Ted30 <teddymarcenaje@gmail.com>
2024-04-26 09:19:53 +0800

    disabled mkcert

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), mkcert()],
+    plugins: [react()],
     resolve: {
         mainFields: [],
     },
     server: {
         proxy: {
             '/api': {
-                target: 'http://202.124.157.19:8000',
+                target: 'http://192.168.137.1:8000',
                 changeOrigin: true,
                 secure: false,
                 // rewrite: (path) => path.replace(/^\/api/, ''),
