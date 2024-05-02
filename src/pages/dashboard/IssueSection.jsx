07b68bdda0c14e7375b366884b10920f366a2e05
Ted30 <teddymarcenaje@gmail.com>
2024-05-02 14:46:33 +0800

    added date filter

--- src/pages/dashboard/IssueSection.jsx
+++ src/pages/dashboard/IssueSection.jsx
@@ -4,7 +4,11 @@ import useHttp from '../../hooks/http-hook'
 
 import styles from './IssueSection.module.css'
 import Dropdown from '../../components/Dropdown/Dropdown'
-import { IconBuilding } from '@tabler/icons-react'
+import { IconBuilding, IconChevronCompactRight } from '@tabler/icons-react'
+import { Box } from '@mui/material'
+import DateSelector from '../../components/DateSelector/DateSelector'
+import dayjs from 'dayjs'
+import { IconChevronRight } from '@tabler/icons-react'
 
 const IssueSection = () => {
     const { sendRequest } = useHttp()
@@ -12,6 +16,8 @@ const IssueSection = () => {
     const [graphData, setGraphData] = useState()
     const [selectedMetric, setSelectedMetric] = useState(1)
     const [reports, setReports] = useState()
+    const [year, setYear] = useState(dayjs)
+    const [month, setMonth] = useState(dayjs)
 
     useEffect(() => {
         const loadData = async () => {
@@ -33,6 +39,47 @@ const IssueSection = () => {
         loadData()
     }, [])
 
+    useEffect(() => {
+        const refetchData = async () => {
+            const res = await sendRequest({
+                url: `${
+                    import.meta.env.VITE_BACKEND_URL
+                }/api/issues_metric/month`,
+                method: 'POST',
+                body: JSON.stringify({
+                    month: dayjs(month).month() + 1,
+                    year: dayjs(month).year(),
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            setGraphData(res)
+        }
+        refetchData()
+    }, [month])
+
+    useEffect(() => {
+        const refetchData = async () => {
+            const res = await sendRequest({
+                url: `${
+                    import.meta.env.VITE_BACKEND_URL
+                }/api/issues_metric/year`,
+                method: 'POST',
+                body: JSON.stringify({
+                    year: dayjs(year).year(),
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            setGraphData(res)
+        }
+        refetchData()
+    }, [year])
+
     const handleSelectMetric = async (e) => {
         if (e.target.value === 1) {
             const res = await sendRequest({
@@ -47,6 +94,29 @@ const IssueSection = () => {
                 url: `${
                     import.meta.env.VITE_BACKEND_URL
                 }/api/issues_metric/month`,
+                method: 'POST',
+                body: JSON.stringify({
+                    month: dayjs(month).month() + 1,
+                    year: dayjs(month).year(),
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            setGraphData(res)
+        } else if (e.target.value === 3) {
+            const res = await sendRequest({
+                url: `${
+                    import.meta.env.VITE_BACKEND_URL
+                }/api/issues_metric/year`,
+                method: 'POST',
+                body: JSON.stringify({
+                    year: dayjs(month).year(),
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             })
 
             setGraphData(res)
@@ -56,18 +126,58 @@ const IssueSection = () => {
 
     return (
         <section className={styles['container']}>
-            <Dropdown
-                leadingIcon={<IconBuilding size={20} color="var(--fc-body)" />}
-                label="Filter"
-                placeholder="Select filter"
-                items={[
-                    { id: 1, name: 'Weekly' },
-                    { id: 2, name: 'Monthly' },
-                ]}
-                value={selectedMetric}
-                selected={selectedMetric}
-                handleSelect={handleSelectMetric}
-            />
+            <Box
+                sx={{
+                    marginTop: '16px',
+                    borderTop: '1px solid var(--border-color)',
+                    paddingTop: '16px',
+                    gap: '8px',
+                    display: 'flex',
+                }}
+            >
+                <Dropdown
+                    leadingIcon={
+                        <IconBuilding size={20} color="var(--fc-body)" />
+                    }
+                    // label="Filter"
+                    placeholder="Select filter"
+                    items={[
+                        { id: 1, name: 'Weekly' },
+                        { id: 2, name: 'Monthly' },
+                        { id: 3, name: 'Yearly' },
+                    ]}
+                    value={selectedMetric}
+                    selected={selectedMetric}
+                    handleSelect={handleSelectMetric}
+                />
+                {(selectedMetric === 2 || selectedMetric === 3) && (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <IconChevronRight />
+                    </Box>
+                )}
+                {selectedMetric === 2 ? (
+                    <DateSelector
+                        currentValue={month}
+                        handleSetValue={setMonth}
+                        defaultValue={null}
+                        label={'Month'}
+                        views={['month', 'year']}
+                    />
+                ) : selectedMetric === 3 ? (
+                    <DateSelector
+                        currentValue={year}
+                        handleSetValue={setYear}
+                        defaultValue={null}
+                        label={'Year'}
+                        views={['year']}
+                    />
+                ) : null}
+            </Box>
 
             {graphData && (
                 <LineChart
